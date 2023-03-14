75a6289d9d6b4596304d08207746074007b64d2f
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-03-14 16:15:18 +0100

    Changes makefile to use bundled tuist

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,9 +4,6 @@ set -e
 
 script_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd "$script_dir/.."
-root_dir=$(pwd)
-
-export PATH=$PATH":$root_dir/.tuist-bin"
 
 defaults write com.apple.dt.Xcode IDEPackageOnlyUseVersionsFromResolvedFile -bool NO
 defaults write com.apple.dt.Xcode IDEDisableAutomaticPackageResolution -bool NO
