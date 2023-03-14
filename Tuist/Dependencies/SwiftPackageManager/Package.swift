10347fbae9a90d6dce4c7201c1da1f5d3e504628
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-03-14 01:06:45 +0100

    Updates dependencies and adds medium and large models

--- Tuist/Dependencies/SwiftPackageManager/Package.swift
+++ Tuist/Dependencies/SwiftPackageManager/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "PackageName",
     dependencies: [
-        .package(url: "https://github.com/Saik0s/AppDevUtils.git", branch: "main"),
+        .package(url: "https://github.com/Saik0s/AppDevUtils.git", from: "0.2.0"),
         .package(url: "https://github.com/Saik0s/OpenAI.git", branch: "main"),
         .package(url: "https://github.com/krzysztofzablocki/Inject.git", branch: "main"),
         .package(url: "https://github.com/KeyboardKit/KeyboardKit.git", from: "6.9.4"),
