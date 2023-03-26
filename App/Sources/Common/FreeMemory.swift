46735b20689b4dedd0f4e595adff68807b9c05f3
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-03-26 08:49:16 +0200

    Add error handling for not enough memory and logging available and free memory in TranscriberClient.swift. Also add a function to calculate free memory and available memory in bytes in Common/FreeMemory.swift, and include a bridging header in Common/Bridging.h

---
+++ App/Sources/Common/FreeMemory.swift
@@ -0,0 +1,27 @@
+import Foundation
+import os
+import OSLog
+
+func freeMemory() -> UInt64 {
+  let size = os_proc_available_memory()
+  return UInt64(size)
+}
+
+func availableMemory() -> UInt64 {
+  let processInfo = ProcessInfo.processInfo
+  let memory = processInfo.physicalMemory
+  return memory
+}
+
+func bytesToReadableString(bytes: UInt64) -> String {
+  let formatter = ByteCountFormatter()
+  formatter.allowedUnits = [.useAll]
+  formatter.countStyle = .memory
+  return formatter.string(fromByteCount: Int64(bytes))
+}
+
+extension UInt64 {
+  var readableString: String {
+    bytesToReadableString(bytes: self)
+  }
+}
