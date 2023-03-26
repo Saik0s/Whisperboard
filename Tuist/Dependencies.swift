f82607fd5b492f587f591a9c1e2b413ab6883885
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-03-26 06:51:56 +0200

    Add support for whisper package and update c++11 language standard and linker flags in Project.swift and Dependencies.swift, respectively

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -2,12 +2,11 @@ import ProjectDescription
 
 let packages: [Package] = [
   .package(url: "https://github.com/Saik0s/AppDevUtils.git", .upToNextMajor(from: "0.2.0")),
-  .package(url: "https://github.com/Saik0s/OpenAI.git", .branch("main")),
   .package(url: "https://github.com/krzysztofzablocki/Inject.git", .branch("main")),
-  .package(url: "https://github.com/KeyboardKit/KeyboardKit.git", .upToNextMajor(from: "6.9.4")),
   .package(url: "https://github.com/dmrschmidt/DSWaveformImage.git", .upToNextMajor(from: "11.0.0")),
-  .package(url: "https://github.com/jasudev/LottieUI.git", .branch("main")),
+  // .package(url: "https://github.com/jasudev/LottieUI.git", .branch("main")),
   .package(url: "https://github.com/AudioKit/AudioKit.git", from: "5.6.0"),
+  .package(url: "https://github.com/ggerganov/whisper.spm", from: "1.2.1"),
 ]
 
 let dependencies = Dependencies(
