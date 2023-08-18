7664bad3741f8b4ef2b54ebcbc6c624035245b58
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-08-19 01:49:11 +0200

    Add FileSystemClient and DataMigrator to handle migrations

--- AppKit/Sources/App/Entities/TranscriptionParameters.swift
+++ AppKit/Sources/App/Entities/TranscriptionParameters.swift
@@ -3,38 +3,104 @@ import Foundation
 // MARK: - WhisperParameters
 
 public struct TranscriptionParameters: Hashable, Codable {
-  var audioContextSize: Int = 0
-  var detectLanguage: Bool = false
-  var durationMilliseconds: Int = 0
-  var enableTokenTimestamps: Bool = false
-  var entropyThreshold: Float = 2.4
-  var greedyBestOf: Int = 2
-  var initialPrompt: String? = nil
-  var language: VoiceLanguage = .auto
-  var lengthPenalty: Float = -1.0
-  var logProbabilityThreshold: Float = -1.0
-  var maxInitialTimestamp: Float = 1.0
-  var maxSegmentLength: Int = 0
-  var maxTextContext: Int = 16384
-  var maxTokensPerSegment: Int = 0
-  var noContext: Bool = true
-  var noSpeechThreshold: Float = 0.6
-  var offsetMilliseconds: Int = 0
-  var printProgressInfo: Bool = true
-  var printRealtimeResults: Bool = false
-  var printSpecialTokens: Bool = false
-  var printTimestamps: Bool = true
-  var shouldTranslate: Bool = false
-  var singleSegmentOutput: Bool = false
-  var speedUpAudio: Bool = false
-  var splitOnWord: Bool = false
-  var suppressBlank: Bool = true
-  var suppressNonSpeechTokens: Bool = false
-  var temperature: Float = 0.0
-  var temperatureIncrease: Float = 0.4
-  var threadCount: Int = min(4, Int(ProcessInfo.processInfo.activeProcessorCount))
-  var timestampTokenProbabilityThreshold: Float = 0.01
-  var timestampTokenSumProbabilityThreshold: Float = 0.01
+  var audioContextSize: Int
+  var detectLanguage: Bool
+  var durationMilliseconds: Int
+  var enableTokenTimestamps: Bool
+  var entropyThreshold: Float
+  var greedyBestOf: Int
+  var initialPrompt: String?
+  var language: VoiceLanguage
+  var lengthPenalty: Float
+  var logProbabilityThreshold: Float
+  var maxInitialTimestamp: Float
+  var maxSegmentLength: Int
+  var maxTextContext: Int
+  var maxTokensPerSegment: Int
+  var noContext: Bool
+  var noSpeechThreshold: Float
+  var offsetMilliseconds: Int
+  var printProgressInfo: Bool
+  var printRealtimeResults: Bool
+  var printSpecialTokens: Bool
+  var printTimestamps: Bool
+  var shouldTranslate: Bool
+  var singleSegmentOutput: Bool
+  var speedUpAudio: Bool
+  var splitOnWord: Bool
+  var suppressBlank: Bool
+  var suppressNonSpeechTokens: Bool
+  var temperature: Float
+  var temperatureIncrease: Float
+  var threadCount: Int
+  var timestampTokenProbabilityThreshold: Float
+  var timestampTokenSumProbabilityThreshold: Float
 
-  public init() {}
+  public init(
+    audioContextSize: Int = 0,
+    detectLanguage: Bool = false,
+    durationMilliseconds: Int = 0,
+    enableTokenTimestamps: Bool = false,
+    entropyThreshold: Float = 2.4,
+    greedyBestOf: Int = 2,
+    initialPrompt: String? = nil,
+    language: VoiceLanguage = .auto,
+    lengthPenalty: Float = -1.0,
+    logProbabilityThreshold: Float = -1.0,
+    maxInitialTimestamp: Float = 1.0,
+    maxSegmentLength: Int = 0,
+    maxTextContext: Int = 16384,
+    maxTokensPerSegment: Int = 0,
+    noContext: Bool = true,
+    noSpeechThreshold: Float = 0.6,
+    offsetMilliseconds: Int = 0,
+    printProgressInfo: Bool = true,
+    printRealtimeResults: Bool = false,
+    printSpecialTokens: Bool = false,
+    printTimestamps: Bool = true,
+    shouldTranslate: Bool = false,
+    singleSegmentOutput: Bool = false,
+    speedUpAudio: Bool = false,
+    splitOnWord: Bool = false,
+    suppressBlank: Bool = true,
+    suppressNonSpeechTokens: Bool = false,
+    temperature: Float = 0.0,
+    temperatureIncrease: Float = 0.4,
+    threadCount: Int = min(4, Int(ProcessInfo.processInfo.activeProcessorCount)),
+    timestampTokenProbabilityThreshold: Float = 0.01,
+    timestampTokenSumProbabilityThreshold: Float = 0.01
+  ) {
+    self.audioContextSize = audioContextSize
+    self.detectLanguage = detectLanguage
+    self.durationMilliseconds = durationMilliseconds
+    self.enableTokenTimestamps = enableTokenTimestamps
+    self.entropyThreshold = entropyThreshold
+    self.greedyBestOf = greedyBestOf
+    self.initialPrompt = initialPrompt
+    self.language = language
+    self.lengthPenalty = lengthPenalty
+    self.logProbabilityThreshold = logProbabilityThreshold
+    self.maxInitialTimestamp = maxInitialTimestamp
+    self.maxSegmentLength = maxSegmentLength
+    self.maxTextContext = maxTextContext
+    self.maxTokensPerSegment = maxTokensPerSegment
+    self.noContext = noContext
+    self.noSpeechThreshold = noSpeechThreshold
+    self.offsetMilliseconds = offsetMilliseconds
+    self.printProgressInfo = printProgressInfo
+    self.printRealtimeResults = printRealtimeResults
+    self.printSpecialTokens = printSpecialTokens
+    self.printTimestamps = printTimestamps
+    self.shouldTranslate = shouldTranslate
+    self.singleSegmentOutput = singleSegmentOutput
+    self.speedUpAudio = speedUpAudio
+    self.splitOnWord = splitOnWord
+    self.suppressBlank = suppressBlank
+    self.suppressNonSpeechTokens = suppressNonSpeechTokens
+    self.temperature = temperature
+    self.temperatureIncrease = temperatureIncrease
+    self.threadCount = threadCount
+    self.timestampTokenProbabilityThreshold = timestampTokenProbabilityThreshold
+    self.timestampTokenSumProbabilityThreshold = timestampTokenSumProbabilityThreshold
+  }
 }
