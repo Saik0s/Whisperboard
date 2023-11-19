64c5b55c48a35eb69d969c7ad27be6fef028b847
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-11-19 20:37:07 +0100

    Increment project version to 1.11.4

--- App/Project.swift
+++ App/Project.swift
@@ -1,7 +1,7 @@
 import Foundation
 import ProjectDescription
 
-let version = "1.11.3"
+let version = "1.11.4"
 
 let projectSettings: SettingsDictionary = [
   "GCC_TREAT_WARNINGS_AS_ERRORS": "YES",
