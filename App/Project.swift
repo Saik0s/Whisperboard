30c82d0fd3c8734548d39b687f44e56b2547553d
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-12-02 19:05:01 +0100

    Bump version to 1.11.5 in Project.swift

--- App/Project.swift
+++ App/Project.swift
@@ -1,7 +1,7 @@
 import Foundation
 import ProjectDescription
 
-let version = "1.11.4"
+let version = "1.11.5"
 
 let projectSettings: SettingsDictionary = [
   "GCC_TREAT_WARNINGS_AS_ERRORS": "YES",
