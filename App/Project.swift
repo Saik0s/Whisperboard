495afbff2d340424aa9978bc14cae57dcae52913
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-12-25 10:50:21 +0100

    🔖 chore(Project.swift): bump project version to 1.11.8

--- App/Project.swift
+++ App/Project.swift
@@ -1,7 +1,7 @@
 import Foundation
 import ProjectDescription
 
-let version = "1.11.7"
+let version = "1.11.8"
 
 let projectSettings: SettingsDictionary = [
   "GCC_TREAT_WARNINGS_AS_ERRORS": "YES",
