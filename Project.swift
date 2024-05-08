8bb501e18016bcd4792623349e45ad7cfb24673d
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2024-05-08 23:39:06 +0200

    ⬆️ (Project.swift): bump project version from 1.12.0 to 1.12.1

--- Project.swift
+++ Project.swift
@@ -1,7 +1,7 @@
 import Foundation
 import ProjectDescription
 
-public let version = "1.12.0"
+public let version = "1.12.1"
 
 public let deploymentTargetString = "16.0"
 public let appDeploymentTargets: DeploymentTargets = .iOS(deploymentTargetString)
